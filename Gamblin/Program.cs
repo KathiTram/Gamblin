ea91976ecff8484c7a585bcf7cc7e1e42f51f111
Kathi <kathitram@gmail.com>
2023-03-13 21:18:32 -0700

    Add project files.

---
+++ Gamblin/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gamblin;
+
+Random random = new Random();
+Double odds = .75;
+Guy player = new Guy() { Cash = 100, Name = "The player" };
+
+
